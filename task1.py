ee3e32f2cda5f5bab13d80910c6c56d65129695c
Huizhong Lee <huizhonglee95@1utar.my>
2016-03-10 23:36:58 +0800

    photo1.png

--- task1.py
+++ task1.py
@@ -1,21 +1,41 @@
 import numpy as np
-#Your optional code here
-#You can import some modules or create additional functions
+ITERATION_LIMIT=10
 
-def lu(A, b):
-    sol = []
-    # Edit here to implement your code
-    return list(sol)
+def LUdecomp(A):
+    n = len(A)
+    for k in range(0, n-1):
+        for i in range(k+1, n):
+            if A[i,k] != 0.0:
+                lam = A[i,k] / A[k,k]
+                A[i, k+1:n] = A[i, k+1:n] - lam * A[k, k+1:n]
+                A[i, k] = lam
+    return A
+
+def lu(A,b):
+    A=LUdecomp(A)
+    n = len(A)
+    for k in range(1,n):
+        b[k] = b[k] - np.dot(A[k,0:k], b[0:k])
+        b[n-1]=b[n-1]/A[n-1, n-1]
+    for k in range(n-2, -1, -1):
+        b[k] = (b[k] - np.dot(A[k,k+1:n], b[k+1:n]))/A[k,k]
+    return b
 
 def sor(A, b):
     sol = []
-    # Edit here to implement your code
+    omega=1.25
+    
+    x = np.zeros_like(b)
+    for itr in range(ITERATION_LIMIT):
+     for i in range(len(b)):
+        sums = np.dot( A[i,:], x )
+        x[i] = x[i] + omega*(b[i]-sums)/A[i,i]
     return list(sol)
 
 def solve(A, b):
-    condition = True # State and implement your condition here
+    condition = np.count_nonzero(A)>1/2*len(A)
     if condition:
-        print('Solve by lu(A,b)')
+        print('Solve by LU(A,b)')
         return lu(A,b)
     else:
         print('Solve by sor(A,b)')
@@ -25,17 +45,28 @@ if __name__ == "__main__":
     ## import checker
     ## checker.test(lu, sor, solve)
 
-    A = [[2,1,6], [8,3,2], [1,5,1]]
-    b = [9, 13, 7]
+    A = np.array([[2,1,6], [8,3,2], [1,5,1]])
+    b = np.array([9, 13, 7])
+    print("Matrix A:")    
+    print(A)
+    print("Matrix b:") 
+    print(b)   
+    print(" ")
     sol = solve(A,b)
     print(sol)
     
-    A = [[6566, -5202, -4040, -5224, 1420, 6229],
+    A = np.array([[6566, -5202, -4040, -5224, 1420, 6229],
          [4104, 7449, -2518, -4588,-8841, 4040],
          [5266,-4008,6803, -4702, 1240, 5060],
          [-9306, 7213,5723, 7961, -1981,-8834],
          [-3782, 3840, 2464, -8389, 9781,-3334],
-         [-6903, 5610, 4306, 5548, -1380, 3539.]]
-    b = [ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]
+         [-6903, 5610, 4306, 5548, -1380, 3539.]])
+    b = np.array([ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906])
+    print(" ")
+    print("Matrix C:")    
+    print(A)
+    print("Matrix d:") 
+    print(b)    
+    print(" ")
     sol = solve(A,b)
     print(sol)
